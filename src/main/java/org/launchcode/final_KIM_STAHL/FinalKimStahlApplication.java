0f1bf952a89676d9a962e9ed5f0debe07993abc7
Kim Stahl <kim.stahl@gmail.com>
2021-06-23 15:21:56 -0500

    new project new commit

---
+++ src/main/java/org/launchcode/final_KIM_STAHL/FinalKimStahlApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.final_KIM_STAHL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalKimStahlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalKimStahlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/final_KIM_STAHL/FinalKimStahlApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.final_KIM_STAHL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalKimStahlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
